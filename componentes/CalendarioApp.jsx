68c458fac12cc5bbf6beb75f0be672e741fd12d7
Demian <demiandiz@gmail.com>
2024-01-18 20:21:34 -0300

    primer subida

---
+++ componentes/CalendarioApp.jsx
@@ -0,0 +1,8 @@
+import '../estilos/CalendarioApp.css'
+
+export const CalendarioApp = ()=> {
+  return (
+    <h1>Calendario</h1>
+  )
+}
+
---
+++ componentes/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {CalendarioApp} from './CalendarioApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CalendarioApp />
+  </React.StrictMode>,
+)
